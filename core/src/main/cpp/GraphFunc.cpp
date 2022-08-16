88bb3650f2d5535d63cd7a58dac8222bf0027ffe
Ariasa <102128722+AriasaProp@users.noreply.github.com>
2022-08-16 09:45:17 +0800

    Delete core/src/main/cpp directory

--- core/src/main/cpp/GraphFunc.cpp
+++
@@ -1,7 +0,0 @@
-#include <jni.h>
-
-#define GraphFunc_M(R, M) extern "C" JNIEXPORT R JNICALL Java_com_ariasaproject_advancerofrpg_GraphFunc_##M
-
-GraphFunc_M(jstring, nativeLog)(JNIEnv *env, jclass clazz) {
-    return env->NewStringUTF("17/05/2022");
-}
